a52eb50b379f5cfbf8d3900e94ba3af0d199f5ca
LRLayan <rameshlayan4@gmail.com>
2024-05-03 00:39:12 +0530

    CrudOperationController

---
+++ src/main/java/com/example/controller/CrudOperationController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CrudOperationController {
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtAge;
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRemoveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+}
