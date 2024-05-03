26b826b789fdf3702c721857495f160691a8b6fb
LRLayan <rameshlayan4@gmail.com>
2024-05-03 13:31:03 +0530

    getAllStudent()

--- src/main/java/com/example/model/StudentModel.java
+++ src/main/java/com/example/model/StudentModel.java
@@ -5,7 +5,10 @@ import com.example.dto.StudentDTO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentModel {
     public boolean saveStudent(StudentDTO studentDTO) throws SQLException {
@@ -17,5 +20,23 @@ public class StudentModel {
         preparedStatement.setString(3,studentDTO.getAddress());
         preparedStatement.setInt(4,studentDTO.getAge());
         return preparedStatement.executeUpdate() > 0;
+    }
+
+    public List<StudentDTO> getAllStudent() throws SQLException {
+        ArrayList <StudentDTO> allStudents = new ArrayList<>();
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM student");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            StudentDTO studentDTO = new StudentDTO(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getInt(4)
+            );
+            allStudents.add(studentDTO);
+        }
+        return allStudents;
     }
 }
