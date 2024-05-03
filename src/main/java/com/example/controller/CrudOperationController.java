48330eba0cda75e0d14c6b8b8accbec6d1e97852
LRLayan <rameshlayan4@gmail.com>
2024-05-03 12:13:40 +0530

    save student

--- src/main/java/com/example/controller/CrudOperationController.java
+++ src/main/java/com/example/controller/CrudOperationController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import com.example.dto.StudentDTO;
+import com.example.model.StudentModel;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 public class CrudOperationController {
     @FXML
@@ -14,9 +17,22 @@ public class CrudOperationController {
     @FXML
     private JFXTextField txtAge;
 
+    StudentModel studentModel = new StudentModel();
+
     @FXML
     void btnNewOnAction(ActionEvent event) {
 
+        var studentDTO = new StudentDTO(0,txtName.getText(),txtAddress.getText(),Integer.parseInt(txtAge.getText()));
+        try{
+           boolean isSaved = studentModel.saveStudent(studentDTO);
+           if (isSaved){
+               new Alert(Alert.AlertType.INFORMATION,"Saved Student!").show();
+           }else {
+               new Alert(Alert.AlertType.ERROR,"Please Try Again").show();
+           }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @FXML
